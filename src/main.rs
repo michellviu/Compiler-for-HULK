988fec883d3fa1743fbc1182eeb608e7f249e5fd
DayanCabrera2003 <dayancorvo@gmail.com>
2025-05-23 13:04:21 -0400

    Add support for single-line and multi-line comments

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,108 @@ use parser::grammar::ProgramParser;
 use parser::visitor::AstPrinterVisitor::AstPrinterVisitor;
 use parser::visitor::Visitable;
 
+fn strip_comments(source: &str) -> Result<String, String> {
+    let mut result = String::with_capacity(source.len());
+    let mut chars = source.chars().peekable();
+    let mut in_multiline_comment = false;
+    let mut multiline_comment_depth = 0;
+
+    while let Some(&c) = chars.peek() {
+        if in_multiline_comment {
+            // Inside multi-line comment, look for end or nested start
+            if c == '*' {
+                chars.next();
+                if let Some(&next_c) = chars.peek() {
+                    if next_c == '/' {
+                        chars.next();
+                        multiline_comment_depth -= 1;
+                        if multiline_comment_depth == 0 {
+                            in_multiline_comment = false;
+                        }
+                        continue;
+                    }
+                }
+            } else if c == '/' {
+                chars.next();
+                if let Some(&next_c) = chars.peek() {
+                    if next_c == '*' {
+                        // Nested multi-line comment detected - error
+                        return Err("Nested multi-line comments are not allowed".to_string());
+                    }
+                }
+            } else {
+                chars.next();
+            }
+        } else {
+            if c == '/' {
+                chars.next();
+                if let Some(&next_c) = chars.peek() {
+                    if next_c == '/' {
+                        // Single-line comment: skip until end of line
+                        chars.next();
+                        while let Some(&line_c) = chars.peek() {
+                            chars.next();
+                            if line_c == '\n' {
+                                result.push('\n');
+                                break;
+                            }
+                        }
+                        continue;
+                    } else if next_c == '*' {
+                        // Start multi-line comment
+                        chars.next();
+                        in_multiline_comment = true;
+                        multiline_comment_depth = 1;
+                        continue;
+                    } else {
+                        // Just a single slash
+                        result.push(c);
+                        continue;
+                    }
+                } else {
+                    // Slash at end of input
+                    result.push(c);
+                    break;
+                }
+            } else {
+                // Normal character, add to result
+                result.push(c);
+                chars.next();
+            }
+        }
+    }
+
+    if in_multiline_comment {
+        return Err("Unterminated multi-line comment".to_string());
+    }
+
+    Ok(result)
+}
+
 fn main() {
-    let expr = ProgramParser::new()
-        .parse("let x=\"hola\" in x;
+    let source = "let x=\"hola\" in x;
+    // This is a single-line comment
     print(x);
-{
-    print(y);
-    let z= 5 in z;
-    };")
+    /*
+    This is a multi-line comment
+    */
+    {
+        print(y);
+        let z= 5 in z;
+    };";
+
+    let cleaned_source = match strip_comments(source) {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!("Error stripping comments: {}", e);
+            return;
+        }
+    };
+
+    let expr = ProgramParser::new()
+        .parse(&cleaned_source)
         .unwrap();
+
     let mut printer = AstPrinterVisitor::new();
     expr.accept(&mut printer);
 }
-
-
-// mod ast;
-// mod error;
-// mod lexer;
-// mod parser;
-
-// use std::env;
-// use std::fs;
-
-//     let source = fs::read_to_string(&args[1]).expect("Failed to read file");
-//     let mut lexer = lexer::Lexer::new(&source);
-//     let tokens = lexer.tokenize().expect("Lexer error");
-//     let mut parser = parser::Parser::new(tokens);
-//     let program = parser.parse_program().expect("Parser error");
-
-//     println!("{:#?}", program);
-// }
