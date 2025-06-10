b95fd2bae483ed6d81351961053b5e07799c5b9d
Eveliz08 <espinaco.eveliz@gmail.com>
2025-06-09 21:13:29 -0400

    Adding AST function call and function declaration

---
+++ src/parser/src/ast/expressions/functiondeclaration.rs
@@ -0,0 +1,45 @@
+use super::super::{Visitable, Visitor};
+use crate::Expression;
+use crate::tokens::{Keyword, Identifier };
+
+#[derive(Debug, PartialEq,Clone)]
+pub struct FunctionParams {
+    pub name: Identifier,
+    pub signature: String,
+}
+
+impl FunctionParams {
+    pub fn new(name: Identifier, signature: String) -> Self {
+        FunctionParams {
+            name,
+            signature,
+        }
+    }
+}
+
+
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct FunctionDef {
+    pub name: Identifier,
+    pub params: Vec<FunctionParams>,
+    pub return_type: String,
+    pub body: Box<Expression>,
+}
+
+impl FunctionDef {
+    pub fn new_expr(name: Identifier, params: Vec<FunctionParams>, return_type: String, expr: Box<Expression>) -> Self {
+        FunctionDef {
+            name,
+            params,
+            return_type,
+            body: expr,
+        }
+    }
+}
+
+impl Visitable for FunctionDef {
+    fn accept<V: Visitor>(&self, visitor: &mut V) {
+        visitor.visit_functdef(self);
+    }
+}
